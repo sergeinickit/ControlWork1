19130cd47c4be863160a7e1e80e460767e1f0b5f
Sergey <sergeinickit@gmail.com>
2024-01-26 01:03:13 +0300

    внес class Program

--- obj/Debug/net8.0/ControlWork1.AssemblyInfo.cs
+++ obj/Debug/net8.0/ControlWork1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControlWork1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c2427e17bca720e7125f2d6ddde1d565d555c9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e964b6b77763de9f20c8a66925d488cb32dd2e83")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControlWork1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ControlWork1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
