302e4bae07525e34412d303164fdf9c321c2e470
Sergey <sergeinickit@gmail.com>
2024-01-26 00:56:18 +0300

    добавил obj

--- obj/Debug/net8.0/ControlWork1.AssemblyInfo.cs
+++ obj/Debug/net8.0/ControlWork1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControlWork1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a602ded138cb1c4418dccd3eb023f49472d85c06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c2427e17bca720e7125f2d6ddde1d565d555c9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControlWork1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ControlWork1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
